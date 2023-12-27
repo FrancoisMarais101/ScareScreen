8763cc616f750af828c4c58469a1d62c9f97ee6d
Francois <francois.marais101@gmail.com>
2023-12-27 17:32:47 +0100

    case update

--- backend/app.py
+++ backend/app.py
@@ -1,26 +1,26 @@
 # app.py
+from os import environ
 from flask import Flask
 from extensions import db
 
 from models.database import *  # Import models to ensure they are registered with SQLAlchemy
-from os import environ
 
 
 def create_app():
     app = Flask(__name__)
 
     # Fetching the Database connection parameters from the environment
-    DB_USER = environ.get("DB_USER")
-    DB_PASS = environ.get("DB_PASS")
-    DB_HOST = environ.get("DB_HOST")
-    DB_PORT = environ.get("DB_PORT")
-    DB_NAME = environ.get("DB_NAME")
+    db_user = environ.get("db_user")
+    db_pass = environ.get("db_pass")
+    db_host = environ.get("db_host")
+    db_port = environ.get("db_port")
+    db_name = environ.get("db_name")
 
     # Creating the SQLAlchemy engine
-    DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    database_url = f"postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
 
     # Configure your app, including database URI and any other settings
-    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_url
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     # Initialize plugins
