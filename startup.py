0a27786cb6c8f09f2ff9a3906f5b23a7b3c6c9fe
Francois <francois.marais101@gmail.com>
2023-12-28 21:00:32 +0100

    add startup.py

--- startup.py
+++ startup.py
@@ -1,9 +1,11 @@
-# startup.py
-
 """
+startup.py
+
 This script is designed to run as part of a Dockerized application setup.
-It waits for the Flask backend service to become available and then makes
-a POST request to the /add_the_witch route to add a movie to the database.
+It waits for the Flask backend service to become available by making
+GET requests to the service's URL. Once the service is available, it
+makes a POST request to the '/add_the_witch' route to add a movie to the
+database.
 """
 
 import time
@@ -14,53 +16,61 @@ def wait_for_server(url, timeout=60):
     """
     Wait for the server to be available by making GET requests to the URL.
 
-    :param url: URL of the server to check.
-    :param timeout: Maximum time in seconds to wait for the server.
-    :raises Exception: If the server is not up within the timeout.
+    Parameters:
+    - url (str): URL of the server to check.
+    - timeout (int): Maximum time in seconds to wait for the server.
+
+    Raises:
+    - Exception: If the server is not up within the timeout period.
     """
     start_time = time.time()
+    request_timeout = 5  # Timeout for each individual request attempt
     while True:
         try:
-            response = requests.get(url)
+            response = requests.get(url, timeout=request_timeout)
             if response.status_code == 200:
                 print("Server is up and running.")
                 break
         except requests.exceptions.ConnectionError:
             print("Server is not yet available. Waiting...")
+        except requests.exceptions.Timeout:
+            print("Server response timed out.")
         except requests.exceptions.RequestException as e:
             print(f"An error occurred: {e}.")
 
-        time_elapsed = time.time() - start_time
-        if time_elapsed > timeout:
+        if time.time() - start_time > timeout:
             raise Exception(f"Server did not start within {timeout} seconds.")
 
-        time.sleep(1)  # wait a second before checking again
+        time.sleep(1)  # Wait a second before retrying
 
 
 def add_movie(url):
     """
     Makes a POST request to the given URL to add a movie to the database.
 
-    :param url: URL to which the POST request will be made.
-    :return: The response text from the POST request.
+    Parameters:
+    - url (str): URL to which the POST request will be made.
+
+    Returns:
+    - str: The response text from the POST request.
     """
+    request_timeout = 5  # Timeout for the post request
     try:
-        response = requests.post(url)
+        response = requests.post(url, timeout=request_timeout)
         return response.text
     except requests.exceptions.RequestException as e:
-        return f"An error occurred while adding the movie: {e}."
+        return f"An error occurred while adding the movie: {e}"
 
 
 def main():
     """
-    Main execution function to wait for the server and add a movie.
+    Main execution function that orchestrates waiting for the server to be available
+    and then adding a movie.
     """
     # URL of the Flask application
-    flask_url = (
-        "http://backend:8000"  # Use the service name as defined in docker-compose
-    )
+    flask_url = "http://backend:8000"
 
-    # Wait for the Flask server to start
+    # Wait for the Flask server to start with the specified timeout
     wait_for_server(flask_url)
 
     # URL for adding the movie
